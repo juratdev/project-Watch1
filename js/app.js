5f963b9cf7de1c24ed85480ac598cffa71984975
Juratdev <juratdev@gmail.com>
2022-01-03 23:33:19 +0500

    Smart Watch

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const toTop = document.querySelector('.top');
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    toTop.classList.add('active');
+  } else {
+    toTop.classList.remove('active');
+  }
+})
